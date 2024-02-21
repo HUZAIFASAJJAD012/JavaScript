9e9f2b57f1be6810a36b6c12a3da94b1ecf9a2e0
huzaifasajjadakbar <140931462+HUZAIFASAJJAD012@users.noreply.github.com>
2024-02-21 12:38:06 +0500

    day 2
    type conversion

---
+++ TASk_2/index.js
@@ -0,0 +1,7 @@
+// type conversion = convert datatype of value to another 
+
+let age = window.prompt("what is your age ");
+age=Number(age);
+age += 1;
+
+console.log("happybrithday", age ,"years old");

4c23212f766455ee1dd4d2c15169aeac8e9efd9d
huzaifasajjadakbar <140931462+HUZAIFASAJJAD012@users.noreply.github.com>
2024-02-21 13:24:04 +0500

    day 2
    math object

--- TASK_3/index.js
+++ TASK_3/index.js
@@ -1,9 +1,22 @@
 // math intransic object 
-let x =24.3;
+// let x =24.3;
 
 
-power=Math.pow(x,3);
-r=Math.round(power);
-f= Math.floor(power);
-window.alert(r);
-window.alert(f);
+// power=Math.pow(x,3);
+// r=Math.round(power);
+// f= Math.floor(power);
+// window.alert(r);
+// window.alert(f);
+
+
+
+let a=1;
+let b=2;
+let c=4;
+
+
+max=Math.max(a,b,c);
+min=Math.min(a,b,c);
+
+window.alert(max);
+window.alert(min);

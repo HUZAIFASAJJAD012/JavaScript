6d69dd252248ecd2c40571eaa62109c70e796334
huzaifasajjadakbar <140931462+HUZAIFASAJJAD012@users.noreply.github.com>
2024-02-21 13:55:48 +0500

    make count number

--- TASK_3/index.js
+++ TASK_3/index.js
@@ -10,13 +10,58 @@
 
 
 
-let a=1;
-let b=2;
-let c=4;
+// let a=1;
+// let b=2;
+// let c=4;
 
 
-max=Math.max(a,b,c);
-min=Math.min(a,b,c);
+// max=Math.max(a,b,c);
+// min=Math.min(a,b,c);
 
-window.alert(max);
-window.alert(min);
+// window.alert(max);
+// window.alert(min);
+
+
+// slove right angled triangle 
+
+document.getElementById("button").onclick= function(){
+
+a=document.getElementById("a").value;
+b=document.getElementById("b").value;
+
+c= Math.sqrt(Math.pow(a,2) + Math.pow(b,2 ));
+window.alert(c);
+
+
+
+}
+var count =5;
+
+document.getElementById("dec").onclick= function(){
+count -=1;
+
+document.getElementById("count").innerHTML =count;
+    
+    
+    }
+
+
+ 
+
+document.getElementById("add").onclick= function(){
+count +=1;
+
+document.getElementById("count").innerHTML =count;
+    
+    
+    }
+
+
+    
+document.getElementById("reset").onclick= function(){
+    
+    count =5;
+    document.getElementById("count").innerHTML =count;
+        
+        
+        }

c0babb707d0a0f67dba2df8373596acd205fbd1d
huzaifasajjadakbar <140931462+HUZAIFASAJJAD012@users.noreply.github.com>
2024-02-23 21:48:39 +0500

    day 3
     radio button

---
+++ TASK_4/index.js
@@ -0,0 +1,38 @@
+document.getElementById("button").onclick =function(){
+
+
+const a=document.getElementById("mycheckbox");
+const b=document.getElementById("mycheckbox1");
+const c=document.getElementById("mycheckbox2");
+
+
+
+if(a.checked){
+
+window.alert("check")
+
+
+}
+else if(b.checked){
+
+    window.alert("check1")
+
+
+}
+
+else if (c.checked){
+
+
+
+    window.alert("check2")
+
+
+}
+
+
+
+
+
+
+
+}

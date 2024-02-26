986ba73e39a81049fb28b0df532b25e92738add6
huzaifasajjadakbar <140931462+HUZAIFASAJJAD012@users.noreply.github.com>
2024-02-26 23:21:23 +0500

    ternary operator

---
+++ TASK_7/index.js
@@ -0,0 +1,44 @@
+// function 
+// mycode();
+
+// function mycode(){
+
+// let age = 4;
+// let name = "huzaifa"
+
+
+
+// my2code(age,name);
+
+
+
+// }
+
+
+
+// function my2code(age,name){
+
+
+// console.log("my  age"+age+"and name is "+name );
+
+
+
+// }
+
+
+// function return some value 
+
+
+
+function my(){
+    let  a=1;
+    let  b=2;
+sum= a+b;
+
+return sum ;
+
+
+
+} 
+
+console.log(my());
---
+++ TASK_8/index.js
@@ -0,0 +1,27 @@
+
+
+//ternary operator 
+
+
+let adult = checkage(20);
+
+console.log(adult);
+function checkage(age){
+
+
+if(age>=18){
+
+return true;
+
+
+}
+else{
+
+
+
+    return false;
+}
+
+
+
+}

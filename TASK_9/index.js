529a5489233914dbc5ba88fb2eebb469ae7dc1af
huzaifasajjadakbar <140931462+HUZAIFASAJJAD012@users.noreply.github.com>
2024-02-28 12:38:41 +0500

    template literals
    //template literals
    // local string

--- TASK_9/index.js
+++ TASK_9/index.js
@@ -1,1 +1,28 @@
-//
+//template literals 
+
+
+let Name = "huzaifa";
+
+let age=20;
+ 
+
+let text =`my name is ${Name}  and my age is ${age}` 
+
+
+
+console.log(text);
+
+
+// local string 
+
+let a=23203392.0;
+
+let b=0.20;
+
+let mynum=a.toLocaleString("en-US",{style:"currency",currency:"PKR"});
+let mynum2=a.toLocaleString("hi-IN");
+let num3=b.toLocaleString(undefined,{style:"unit",unit:"celsius"});
+console.log(mynum);
+console.log(mynum2);
+console.log(num3);
+
